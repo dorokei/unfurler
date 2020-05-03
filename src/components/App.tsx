9365262b92ce5a5a49de912bbcefaa089993d70c
小野 慶太 <dorokei@gmail.com>
2020-05-04 03:14:04 +0900

    Simple design

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,8 +3,17 @@ import * as React from "react";
 class App extends React.Component {
   render() {
     return (
-      <div>App</div>
-    )
+      <div className="field has-addons">
+        <div className="control is-expanded">
+          <input className="input is-large" type="text" placeholder="Paste a URL" />
+        </div>
+        <div className="control">
+          <a className="button is-info  is-large">
+            Search
+          </a>
+        </div>
+      </div>
+    );
   }
 }
 
